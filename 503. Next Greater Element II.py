2c7838e40bb3043e4282daf6d6740743c3a65cef
jimmyjohn0315 <47842500+jimmyjohn0315@users.noreply.github.com>
2021-12-17 00:50:36 -0600

    Create 503. Next Greater Element II.py

---
+++ 503. Next Greater Element II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [-1] * n
+        stack = []
+        for i in range(2 * n):
+            while stack and nums[i % n] > nums[stack[-1]]:
+                cur = stack.pop()
+                res[cur] = nums[i % n]
+            stack.append(i % n)
+        return res
